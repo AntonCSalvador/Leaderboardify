01f39c234cfe4423369bd8b3f1d2fcccf2f236c6
AntonCSalvador <antoncsalvador@Gmail.com>
2023-08-24 14:34:17 -0400

    added timer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import logo from './logo.svg';
-import leaderboardifyLogo from './leaderboardify.png'; // Import the image
+import leaderboardifyLogo from './leaderboardifyv2.png'; // Import the image
 import './App.scss';
 import topArtistsData from './top_artists.json'; // Adjust the path as needed
+import React, { useState, useEffect } from 'react';
+
 
 function App() {
+  const [remainingTime, setRemainingTime] = useState(24 * 60 * 60); // 24 hours in seconds and keeps the variable consistent with useState
+
+  useEffect(() => { //updates time 
+    const interval = setInterval(() => {
+      setRemainingTime(prevTime => (prevTime > 0 ? prevTime - 1 : 0));
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className = "outerWrap">
       <div className="App">
@@ -17,6 +29,16 @@ function App() {
             <li className = "active">Leaderboard</li>
             <li>Your Library</li>
           </ul>
+          <div className="timer">
+            <li>Next Reset In:</li>
+            <div className="timer-value">
+              <li>
+              {Math.floor(remainingTime / 3600)}h&nbsp;
+              {Math.floor((remainingTime % 3600) / 60)}m&nbsp;
+              {remainingTime % 60}s
+              </li>
+            </div>
+          </div>
           <div className="note">
             <span>I did not create this design</span>
             <span>COMPLETELY BASED OFF SPOTIFY</span>
